aa36a8902e0f81af96180e4878d339c391abe52c
jleonreg <jleonreg@alumnos.unex.es>
2022-10-24 17:16:40 +0200

    Creacion resto activity

---
+++ app/src/main/java/es/unex/dinopedia/CombateResulActivity.java
@@ -0,0 +1,14 @@
+package es.unex.dinopedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CombateResulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combate_resul);
+    }
+}
