fc54277f8d70eb3fb18c2bb2a794a246cd94f48b
Carmen <cmartinue@alumnos.unex.es>
2022-10-24 17:01:21 +0200

    prueba push carmen

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
-
+//carmen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
