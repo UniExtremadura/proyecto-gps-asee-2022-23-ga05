822f445d08344b1536642fd7c6bbfe2eaa5a949c
JesusSF <jesantosf@alumnos.unex.es>
2022-10-24 17:04:28 +0200

    prueba push gsus

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity{
 
 //carmen
+    //G-sus
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
