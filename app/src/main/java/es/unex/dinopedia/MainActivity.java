b1b4287f97ea1f540407b584f24171405acd4ae8
jleonreg <jleonreg@alumnos.unex.es>
2022-10-27 12:06:04 +0200

    Creación proyecto

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -20,26 +20,5 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
-        Button bCombate = findViewById(R.id.bCombate);
-
-        bEnciclopedia.setOnClickListener(view -> {
-            //Snackbar.make(view, "Remplaza con una acción", Snackbar.LENGTH_LONG)
-            //       .setAction("Action", null).show();
-            Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
-            startActivity(intent);
-        });
-
-
-        bCombate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Snackbar.make(view, "Remplaza con una acción", Snackbar.LENGTH_LONG)
-                //       .setAction("Action", null).show();
-                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
