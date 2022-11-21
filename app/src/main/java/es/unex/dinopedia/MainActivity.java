090d8f6384b77213457a0b02c0735481333456ba
jleonreg <jleonreg@alumnos.unex.es>
2022-11-21 18:58:37 +0100

    Caso Base Proyecto Dinopedia

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -1,24 +1,35 @@
 package es.unex.dinopedia;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
 
-//carmen
-    //G-sus
-    // Belén
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
+        Button bCombate = findViewById(R.id.bCombate);
+
+        bCombate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bEnciclopedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
