3d007d986d589d1478f195590f604ee5c14f9c22
Carmen <cmartinue@alumnos.unex.es>
2022-11-21 19:43:46 +0100

    PGA05-87 UC01 - IT2 - IMP - Implement Components

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -7,109 +7,29 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import es.unex.dinopedia.roomdb.DinosaurioDatabase;
-import es.unex.dinopedia.roomdb.LogroDatabase;
-
 public class MainActivity extends AppCompatActivity{
-
-    DinosaurioAdapter mAdapter = new DinosaurioAdapter(MainActivity.this, new DinosaurioAdapter.OnItemClickListener() {
-        @Override public void onItemClick(Dinosaurio item) {
-            //Snackbar.make(view, "Item "+item.getName()+" Clicked", Snackbar.LENGTH_LONG).show();
-        }
-    });
-
-    List<Dinosaurio> dino = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
+        Button bCombate = findViewById(R.id.bCombate);
 
+        bCombate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        AppExecutors.getInstance().diskIO().execute(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        LogroDatabase.getInstance(MainActivity.this).getDao().deleteAll();
-
-                        if (LogroDatabase.getInstance(MainActivity.this).getDao().count() == 0) {
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.logros)));
-                            String receiveString = "";
-                            StringBuilder stringBuilder = new StringBuilder();
-
-                            while (true) {
-                                try {
-                                    if (!((receiveString = bufferedReader.readLine()) != null))
-                                        break;
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                                stringBuilder.append("\n").append(receiveString);
-                            }
-
-                            String read = stringBuilder.toString();
-
-                            List<Logro> logro = Arrays.asList(new Gson().fromJson(read, Logro[].class));
-                            for (int i = 0; i < logro.size(); i++) {
-                                Logro l = logro.get(i);
-                                LogroDatabase.getInstance(MainActivity.this).getDao().insert(l);
-                            }
-                        }
-
-
-                        AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                            @Override
-                            public void run() {
-                                //DinosaurioDatabase.getInstance(MainActivity.this).getDao().deleteAll();
-
-
-                                if (DinosaurioDatabase.getInstance(MainActivity.this).getDao().count() == 0) {
-                                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.jurassicpark)));
-                                    String receiveString = "";
-                                    StringBuilder stringBuilder = new StringBuilder();
-
-                                    while (true) {
-                                        try {
-                                            if (!((receiveString = bufferedReader.readLine()) != null))
-                                                break;
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
-                                        stringBuilder.append("\n").append(receiveString);
-                                    }
-
-                                    String read = stringBuilder.toString();
-
-                                    List<Dinosaurio> dino = Arrays.asList(new Gson().fromJson(read, Dinosaurio[].class));
-                                    for (int i = 0; i < dino.size(); i++) {
-                                        Dinosaurio d = dino.get(i);
-                                        DinosaurioDatabase.getInstance(MainActivity.this).getDao().insert(d);
-                                    }
-                                }
-                            }
-                        });
-
-                        AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                            @Override
-                            public void run() {
-                                DinosaurioDatabase database = DinosaurioDatabase.getInstance(MainActivity.this);
-                                dino = database.getDao().getAll();
-                                if (dino.size() != 0) {
-                                    AppExecutors.getInstance().mainThread().execute(() -> mAdapter.load(dino));
-                                }
-                            }
-                        });
-
-                    }
-                });
+        bEnciclopedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
