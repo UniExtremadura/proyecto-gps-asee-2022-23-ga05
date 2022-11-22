6d5bfa5ec3704b953e61f6c857643b890efb58db
JesusSF <jesantosf@alumnos.unex.es>
2022-11-22 03:12:31 +0100

    PGA05-94 UC11 - IT7 - IMP - Implement Components

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -1,35 +1,124 @@
 package es.unex.dinopedia;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import es.unex.dinopedia.databinding.ActivityMainBinding;
+import es.unex.dinopedia.roomdb.DinosaurioDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    DinosaurioAdapter mAdapter = new DinosaurioAdapter(MainActivity.this, new DinosaurioAdapter.OnItemClickListener() {
+        @Override public void onItemClick(Dinosaurio item) {
+            //Snackbar.make(view, "Item "+item.getName()+" Clicked", Snackbar.LENGTH_LONG).show();
+        }
+    });
+    List<Dinosaurio> dino = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
-        Button bCombate = findViewById(R.id.bCombate);
 
-        bCombate.setOnClickListener(new View.OnClickListener() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
-                startActivity(intent);
+            public void run() {
+                //DinosaurioDatabase.getInstance(MainActivity.this).getDao().deleteAll();
+
+
+                if (DinosaurioDatabase.getInstance(MainActivity.this).getDao().count() == 0) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.jurassicpark)));
+                    String receiveString = "";
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    while (true) {
+                        try {
+                            if (!((receiveString = bufferedReader.readLine()) != null)) break;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        stringBuilder.append("\n").append(receiveString);
+                    }
+
+                    String read = stringBuilder.toString();
+
+                    List<Dinosaurio> dino = Arrays.asList(new Gson().fromJson(read, Dinosaurio[].class));
+                    for (int i = 0; i < dino.size(); i++) {
+                        Dinosaurio d = dino.get(i);
+                        DinosaurioDatabase.getInstance(MainActivity.this).getDao().insert(d);
+                    }
+                }
             }
         });
 
-        bEnciclopedia.setOnClickListener(new View.OnClickListener() {
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
-                startActivity(intent);
+            public void run() {
+                DinosaurioDatabase database = DinosaurioDatabase.getInstance(MainActivity.this);
+                dino = database.getDao().getAll();
+                if(dino.size()!=0){
+                    AppExecutors.getInstance().mainThread().execute(()->mAdapter.load(dino));
+                }
             }
         });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MainFragment mF = new MainFragment(MainActivity.this, binding);
+        replaceFragment(mF);
+
+        EnciclopediaFragment eF = new EnciclopediaFragment(MainActivity.this);
+        FavoritoFragment fF = new FavoritoFragment(MainActivity.this);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.principal:
+                    replaceFragment(mF);
+                    break;
+                case R.id.enciclopedia:
+                    replaceFragment(eF);
+                    break;
+                case R.id.batalla:
+                    replaceFragment(new CombateFragment());
+                    break;
+                case R.id.favorito:
+                    replaceFragment(fF);
+                    break;
+                case R.id.logros:
+                    replaceFragment(new AlbumFragment());
+                    break;
+            }
+            return true;
+        });
+
+
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
