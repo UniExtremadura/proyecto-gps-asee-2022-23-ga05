85f7b505c32b6a17adb06acb39a2fc7647a0c89e
jleonreg <jleonreg@alumnos.unex.es>
2022-11-28 18:49:23 +0100

    Develop reentrega

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -1,6 +1,9 @@
 package es.unex.dinopedia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
+        Button bCombate = findViewById(R.id.bCombate);
+
+        bCombate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bEnciclopedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
