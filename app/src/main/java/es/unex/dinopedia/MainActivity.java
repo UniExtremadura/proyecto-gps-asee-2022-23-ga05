7a5e931e76dbba99f87de196a09ecddf772950a6
Carmen <cmartinue@alumnos.unex.es>
2022-11-29 09:56:18 +0100

    PGA05-121 UC05 - IT3 - IMP - Implement Components

--- app/src/main/java/es/unex/dinopedia/MainActivity.java
+++ app/src/main/java/es/unex/dinopedia/MainActivity.java
@@ -1,35 +1,63 @@
 package es.unex.dinopedia;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import es.unex.dinopedia.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity{
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bEnciclopedia = findViewById(R.id.bEnciclopedia);
-        Button bCombate = findViewById(R.id.bCombate);
 
-        bCombate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CombateActivity.class);
-                startActivity(intent);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MainFragment mF = new MainFragment();
+        replaceFragment(mF);
+
+        EnciclopediaFragment eF = new EnciclopediaFragment();
+        FavoritoFragment fF = new FavoritoFragment();
+        CombateFragment cF = new CombateFragment();
+        AlbumFragment aF = new AlbumFragment();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.principal:
+                    replaceFragment(mF);
+                    break;
+                case R.id.enciclopedia:
+                    replaceFragment(eF);
+                    break;
+                case R.id.batalla:
+                    replaceFragment(cF);
+                    break;
+                case R.id.favorito:
+                    replaceFragment(fF);
+                    break;
+                case R.id.logros:
+                    replaceFragment(aF);
+                    break;
             }
+            return true;
         });
 
-        bEnciclopedia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EnciclopediaActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
