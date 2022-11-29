556b3ac269c00ef85f2266790c137ad93ef31c00
JesusSF <jesantosf@alumnos.unex.es>
2022-11-30 00:00:38 +0100

    PGA05-67 UC07 - IT4 - IMP - Implement Components

--- app/src/main/java/es/unex/dinopedia/MainFragment.java
+++ app/src/main/java/es/unex/dinopedia/MainFragment.java
@@ -1,61 +1,127 @@
 package es.unex.dinopedia;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import es.unex.dinopedia.databinding.ActivityMainBinding;
+import es.unex.dinopedia.roomdb.DinosaurioDatabase;
+
+
 public class MainFragment extends Fragment {
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View vista;
+    private Context context;
+    ActivityMainBinding binding;
+    private DinosaurioAdapter mAdapter;
+    private DateFormat formatoFecha;
+    private Date fecha;
+    private int dinosaurioDelDia;
 
-    private String mParam1;
-    private String mParam2;
+    private List<Dinosaurio> dinoList;
+    private List<Dinosaurio> copiaDinosaurio;
 
-    public MainFragment() {
-        // Required empty public constructor
+    public MainFragment(){
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+    public MainFragment(Context cont, ActivityMainBinding bind) {
+        context = cont;
+        dinoList=new ArrayList<>();
+        mAdapter = new DinosaurioAdapter(context, new DinosaurioAdapter.OnItemClickListener() {
+            @Override public void onItemClick(Dinosaurio item) {
+            }
+        });
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                DinosaurioDatabase database = DinosaurioDatabase.getInstance(context);
+                dinoList = database.getDao().getAll();
+                AppExecutors.getInstance().mainThread().execute(()->copiaDinosaurio=dinoList);
+            }
+        });
+        formatoFecha = new SimpleDateFormat("dd/MM/yy");
+        long momento = System.currentTimeMillis();
+        fecha = new Date(momento);
+        binding = bind;
+
+        if(copiaDinosaurio!=null) {
+            if (copiaDinosaurio.size() != 0) {
+                Random random_method = new Random();
+                dinosaurioDelDia = random_method.nextInt(copiaDinosaurio.size());
+            } else {
+                dinosaurioDelDia = 0;
+            }
+        }
+    }
+
+
+    public static MainFragment newInstance(Context cont, ActivityMainBinding bind) {
+        MainFragment fragment = new MainFragment(cont, bind);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        vista = rootView;
+        return rootView;
+    }
+
+    public void elegirDinosaurio(){
+        String nombre;
+        Random random_method = new Random();
+        if(copiaDinosaurio!=null) {
+            if (copiaDinosaurio.size() != 0) {
+                int nuevoDinosaurio = random_method.nextInt(copiaDinosaurio.size());
+                long momento = System.currentTimeMillis();
+                Date fechaActual = new Date(momento);
+                String fechaActualS = formatoFecha.format(fechaActual);
+                String fechaS = formatoFecha.format(fecha);
+                if (!fechaS.equals(fechaActualS)) {
+                    fecha = fechaActual;
+                    nuevoDinosaurio = random_method.nextInt(copiaDinosaurio.size());
+                    Log.d("fecha", fechaS);
+                    dinosaurioDelDia = nuevoDinosaurio;
+                }
+                Dinosaurio d = dinoList.get(dinosaurioDelDia);
+                Log.d("HOLA", d.getName());
+                nombre = d.getName();
+
+                if (dinoList.size() != 0) {
+                    final TextView dinoDia = (TextView) vista.findViewById(R.id.nombreDino);
+                    dinoDia.setText(nombre);
+                }
+            }
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public void onResume() {
+        super.onResume();
+
+            if(dinoList.size()!=0){
+                elegirDinosaurio();
+            }
+
     }
 }
