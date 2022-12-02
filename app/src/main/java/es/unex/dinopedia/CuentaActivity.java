fb056adb8d0795bcbd83f97a5c6cd396296e0419
jleonreg <jleonreg@alumnos.unex.es>
2022-12-02 18:11:47 +0100

    PGA05-151 UC08 - IT4 - IMP- Implement Components

--- app/src/main/java/es/unex/dinopedia/CuentaActivity.java
+++ app/src/main/java/es/unex/dinopedia/CuentaActivity.java
@@ -1,7 +1,16 @@
 package es.unex.dinopedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import es.unex.dinopedia.roomdb.UsuarioDatabase;
 
 public class CuentaActivity extends AppCompatActivity {
 
@@ -9,8 +18,26 @@ public class CuentaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta);
-    }
+        Button bAyuda = findViewById(R.id.bAyuda);
+        Button bContacto = findViewById(R.id.bContactar);
 
+        bAyuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CuentaActivity.this, AyudaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CuentaActivity.this, ContactoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
     @Override
     public void onResume(){
         super.onResume();
