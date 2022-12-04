9f04679575f9601158a5be1a72ae1e0207660e3c
BmRg8 <bramirezkp@alumnos.unex.es>
2022-12-04 16:55:36 +0100

    PGA05-85 UC14 - IT7 - IMP - Implement Components

--- app/src/main/java/es/unex/dinopedia/DinosaurioInfoActivity.java
+++ app/src/main/java/es/unex/dinopedia/DinosaurioInfoActivity.java
@@ -4,11 +4,14 @@ import static es.unex.dinopedia.R.*;
 import static es.unex.dinopedia.R.id.*;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 import es.unex.dinopedia.roomdb.DinosaurioDatabase;
+import es.unex.dinopedia.roomdb.UsuarioDatabase;
 
 public class DinosaurioInfoActivity extends AppCompatActivity {
 
@@ -20,6 +23,7 @@ public class DinosaurioInfoActivity extends AppCompatActivity {
     private TextView tPeriodNameD;
     private TextView tLengthMetersD;
     private Bundle bundle;
+    private Button bFavorite;
     private List<Dinosaurio> dinoList = new ArrayList<>();
 
     @Override
@@ -29,12 +33,39 @@ public class DinosaurioInfoActivity extends AppCompatActivity {
         bundle = getIntent().getExtras();
         View v = this.findViewById(android.R.id.content);
 
+        Switch swFavorito = findViewById(R.id.sFavorito);
+
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
                 DinosaurioDatabase database = DinosaurioDatabase.getInstance(DinosaurioInfoActivity.this);
                 Dinosaurio d = database.getDao().getDinosaurioId(bundle.getLong("id"));
                 AppExecutors.getInstance().mainThread().execute(()->actualizarDinosaurio(d));
+            }
+        });
+
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                UsuarioDatabase database = UsuarioDatabase.getInstance(DinosaurioInfoActivity.this);
+                if(database.getDao().getUsuario()!=null)
+                    swFavorito.setVisibility(v.VISIBLE);
+            }
+        });
+
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                DinosaurioDatabase database = DinosaurioDatabase.getInstance(DinosaurioInfoActivity.this);
+                Dinosaurio d = database.getDao().getDinosaurioId(bundle.getLong("id"));
+                if(d.getFavorite().equals("0")){
+                    AppExecutors.getInstance().mainThread().execute(()->swFavorito.setChecked(false));
+                }
+                else{
+                    AppExecutors.getInstance().mainThread().execute(()->swFavorito.setChecked(true));
+                }
+                AppExecutors.getInstance().mainThread().execute(()->actualizarDinosaurio(d));
+                AppExecutors.getInstance().mainThread().execute(()->cambiarFavorito(d));
             }
         });
     }
@@ -55,5 +86,29 @@ public class DinosaurioInfoActivity extends AppCompatActivity {
         tSpeciesD.setText(d.getSpecies());
         tPeriodNameD.setText(d.getPeriodName());
         tLengthMetersD.setText(d.getLengthMeters());
+    }
+
+    public void cambiarFavorito(Dinosaurio d){
+        DinosaurioDatabase database = DinosaurioDatabase.getInstance(DinosaurioInfoActivity.this);
+
+        Switch swFavorito = findViewById(R.id.sFavorito);
+
+        swFavorito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    if(swFavorito.isChecked()){
+                        d.setFavorite("1");
+                        database.getDao().update(d);
+                    }
+                    else{
+                        d.setFavorite("0");
+                        database.getDao().update(d);
+                    }
+                }
+            });
+            }
+        });
     }
 }
